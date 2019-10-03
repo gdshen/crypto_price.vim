615f765d2993b48d7b0e668905612d914af1b59d
gdshen <gdshen95@gmail.com>
2019-10-03 13:45:35 +0000

    add except handle

--- rplugin/python3/price.py
+++ rplugin/python3/price.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+from time import sleep
+
 import pynvim
 import requests
 
@@ -11,8 +14,22 @@ class Price(object):
     def stock_function_handler(self, args):
         url = "http://api.coincap.io/v2/assets/bitcoin"
         params = {"exchangeId": "okex", "baseSymbol": "BTC", "quoteSymbol": "USDT"}
+        try:
+            response = requests.get(url, params=params)
+            data = response.json()
+            price = int(float(data["data"]["priceUsd"]))
+            # use command to define var
+            self.vim.command("let g:btc_price={}".format(price))
+        except Exception as e:
+            self.vim.command("let g:btc_price=-1")
+
+
+if __name__ == "__main__":
+    url = "http://api.coincap.io/v2/assets/bitcoin"
+    params = {"exchangeId": "okex", "baseSymbol": "BTC", "quoteSymbol": "USDT"}
+    while True:
         response = requests.get(url, params=params)
         data = response.json()
         price = int(float(data["data"]["priceUsd"]))
-        # use command to define var
-        self.vim.command("let g:btc_price={}".format(price))
+        print("{} {}".format(datetime.now(), price))
+        sleep(1)
